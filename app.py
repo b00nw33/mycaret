4df4d692123541d1b8c4819351e80b326f40b029
b00nw33 <33589369+b00nw33@users.noreply.github.com>
2023-09-11 14:18:13 +0800

    Proceed to train models

--- app.py
+++ app.py
@@ -11,3 +11,14 @@ with open("metrics.txt", "w") as outfile:
     outfile.write("Before data.info():\n")
     data.info()
     outfile.write("After data.info()")
+
+numeric = ['age', 'bmi', 'children', 'charges']
+categorical = ['smoker', 'sex', 'region']
+color = ['C0', 'C1', 'C2', 'C3']
+cols = ['age', 'bmi', 'charges', 'smoker']
+
+reg = setup(data=data, target='charges', train_size = 0.8, session_id = 7402,
+            numeric_features = numeric[:-1], categorical_features = categorical,
+            transformation = True, normalize = True)
+
+best = compare_models(sort='RMSE')
